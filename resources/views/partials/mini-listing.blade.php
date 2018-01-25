8c60a4a2a973963a1084d205007265f2ef499ad2
programmer-man <bryan@kerigan.com>
2018-01-25 11:33:42 -0500

    added search template and started search bar

--- resources/views/partials/mini-listing.blade.php
+++ resources/views/partials/mini-listing.blade.php
@@ -1,6 +1,6 @@
 <div class="w-full md:w-1/2 lg:w-1/3 xl:w-1/4 px-2 py-2" >
     <div class="property relative bg-white shadow-md md:rounded overflow-hidden">
-        <a href="#" class="absolute pin hover:shadow-inner" ></a>
+        <a href="/listing/123456/" class="absolute pin hover:shadow-inner" ></a>
         <div class="property-image md:h-48 md:overflow-hidden">
             <img class="w-auto h-auto min-h-full min-w-full" src="https://cloud.netlifyusercontent.com/assets/344dbf88-fdf9-42bb-adb4-46f01eedd629/160ae39d-b5b0-4d9f-a8e1-76102113e2f8/placeholder-picture-large-opt.png" >
         </div>
