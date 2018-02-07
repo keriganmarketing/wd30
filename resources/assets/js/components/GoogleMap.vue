82eb59cf514ffad5484ad74d7fb8d4cf268a8f76
Daron Adkins <daron.adkins@gmail.com>
2018-02-06 21:56:09 -0600

    merge with master

--- resources/assets/js/components/GoogleMap.vue
+++ resources/assets/js/components/GoogleMap.vue
@@ -1,37 +1,100 @@
 <template>
-    <div class="google-map w-full h-full" ref="map" />
+    <div class="google-map w-full h-full" ref="map">
+        <div
+            ref="directionsButton"
+            class="w-1/5 p-4 flex border-white justify-center items-center rounded absolute z-99 bg-brand-light text-lg rounded text-white text-center"
+            @click="getDirections"
+        >
+            GET DIRECTIONS
+        </div>
+        <div
+            ref="directionsPanel"
+            class="w-full h-full bg-white z-99 relative text-black pl-2 relative absolute overflow-auto"
+            v-html="directionsPanel"
+        />
+    </div>
 </template>
 
 <script>
-import GoogleMapsLoader from 'google-maps';
-import { loadMap } from '../google-maps.service.js';
+import GeoLocator from '../services/geolocator.service.js';
+import GoogleMap from '../services/google-maps.service.js';
 export default {
     props: {
-        name: {
-            type: String,
-            default: this.name
-        },
         latitude: {
             type: Number,
-            default: 0
+            default: this.latitude
         },
         longitude: {
             type: Number,
-            default: 0
+            default: this.longitude
         },
         zoom: {
             type: Number,
-            default: 100
+            default: this.zoom
+        }
+    },
+    data () {
+        return {
+            renderedMap: {},
+            myPosition: {},
+            error: '',
+            directionsPanel: ''
         }
     },
     mounted () {
-        let config = {
-            element: this.$refs.map,
-            zoom: this.zoom,
-            lat: this.latitude,
-            long: this.longitude
-        };
-        loadMap(config);
+        this.getUserLocation();
+        this.renderMap();
+    },
+    methods: {
+        getUserLocation() {
+            let vm = this;
+            let geo = new GeoLocator();
+            geo.getLocation()
+                .then(position => {
+                    vm.myPosition = position;
+                })
+                .catch(error => {
+                    vm.error = error.message;
+                })
+        },
+        renderMap() {
+            let config = {
+                zoom:    this.zoom,
+                origin:  this.myPosition,
+                mapElement: this.$refs.map,
+                destination: {
+                    latitude: this.latitude,
+                    longitude: this.longitude
+                },
+                directionsButton: this.$refs.directionsButton,
+                directionsPanel: this.$refs.directionsPanel
+            };
+            new GoogleMap(config)
+                .load()
+                .then(rendered => {
+                    this.renderedMap = rendered;
+                });
+        },
+        getDirections () {
+            let config = {
+                zoom:    this.zoom,
+                origin:  this.myPosition,
+                mapElement: this.$refs.map,
+                destination: {
+                    latitude: this.latitude,
+                    longitude: this.longitude
+                },
+                directionsButton: this.$refs.directionsButton,
+                directionsPanel: this.$refs.directionsPanel
+            };
+            let locations = {
+                origin: config.origin,
+                destination: config.destination,
+            };
+            let directions = new GoogleMap(config)
+                .getDirections(locations, this.renderedMap, this.$refs.directionsButton, this.$refs.directionsPanel);
+            console.log(directions);
+        }
     }
 }
 </script>
---
+++ resources/assets/js/services/google-maps.service.js
@@ -0,0 +1,62 @@
+import GoogleMapsLoader from 'google-maps';
+
+export default class GoogleMap {
+    constructor (config) {
+        this.config = config;
+        this.map = {};
+    }
+    load () {
+        return new Promise((resolve, reject = null) => {
+            resolve(this.render());
+        });
+    }
+    render () {
+        GoogleMapsLoader.KEY = 'AIzaSyCRXeRhZCIYcKhtc-rfHCejAJsEW9rYtt4';
+        let config = this.config;
+        let mapData = this.map;
+        GoogleMapsLoader.load(google => {
+            mapData.map = new google.maps.Map(config.mapElement, {
+                zoom: config.zoom,
+                center: new google.maps.LatLng(config.destination.latitude, config.destination.longitude),
+                disableDefaultUI: true,
+                zoomControl: true,
+                scaleControl: true
+            });
+
+            let control = config.directionsButton;
+            mapData.map.controls[google.maps.ControlPosition.TOP_LEFT].push(control);
+
+            mapData.position = mapData.map.center;
+            mapData.marker = new google.maps.Marker({
+                position: mapData.position,
+                map: mapData.map
+            });
+        });
+
+        return this.map;
+    }
+    getDirections(locations, mapData, button, panel) {
+        GoogleMapsLoader.KEY = 'AIzaSyCRXeRhZCIYcKhtc-rfHCejAJsEW9rYtt4';
+        GoogleMapsLoader.load(google => {
+            let directionsDisplay = new google.maps.DirectionsRenderer({
+                origin: new google.maps.LatLng(locations.origin.latitude, locations.origin.longitude),
+                destination: new google.maps.LatLng(locations.destination.latitude, locations.destination.longitude),
+                travelMode: 'DRIVING',
+            });
+            let directionsService = new google.maps.DirectionsService;
+            directionsDisplay.setMap(mapData.map);
+            directionsDisplay.setPanel(panel);
+            directionsService.route({
+                origin: directionsDisplay.origin,
+                destination: directionsDisplay.destination,
+                travelMode: 'DRIVING'
+            }, function (response, status) {
+                if (status === 'OK') {
+                    return directionsDisplay.setDirections(response);
+                } else {
+                    return window.alert('Directions request failed due to ' + status)
+                }
+            });
+        });
+    }
+}
