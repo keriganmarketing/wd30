93546eb0c1bbe3cea17cd307ee5af78eb2fb625a
Dylan <johnston94@gmail.com>
2018-02-07 21:29:15 -0600

    Update Welcome Card

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,18 +1,8 @@
 @extends('layouts.dashboard')
 
 @section('content')
-<div class="container flex flex-wrap mx-auto mb-8 justify-between">
-    <div class="w-full flex justify-around h-full">
-        <div class="w-full md:w-1/3 bg-white">
-            <avatar-upload :avatar-path='"{{ $avatarPath }}"'></avatar-upload>
-        </div>
-        <div class="w-full md:w-2/3">
-            <bio-card :data-user="user" v-on:submit-button-clicked="sbc"></bio-card>
-        </div>
-    </div>
-    <div class="w-full h-full">
-        <welcome-card :data-user="user"></welcome-card>
-    </div>
+<div class="container flex flex-wrap bg-tan-lightest mx-auto mb-8 justify-between">
+    <welcome-card :data-user="user" :avatar-path='"{{ $avatarPath }}"'></welcome-card>
 </div>
 <leads></leads>
 @endsection
