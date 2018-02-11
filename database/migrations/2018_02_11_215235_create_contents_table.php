975c0c927ee10f1d0279e0603e3c097ab70d5756
Daron Adkins <daron.adkins@gmail.com>
2018-02-11 17:43:42 -0600

    start content card

---
+++ database/migrations/2018_02_11_215235_create_contents_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateContentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contents', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title')->nullable()->default('The tagline for your site');
+            $table->longText('body')->nullable()->default('Here is where you will tell the world why they should purchase or list a home with you');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contents');
+    }
+}
