111ca1a33a26fd823cd28142c9285c795e412b23
Daron Adkins <daron.adkins@gmail.com>
2018-02-21 00:59:42 -0600

    SEO generation and sitemap

--- app/Http/Controllers/FrontPageController.php
+++ app/Http/Controllers/FrontPageController.php
@@ -2,10 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use SEOMeta;
 use App\User;
+use App\Content;
 use Illuminate\Http\Request;
 use Facades\KeriganSolutions\Drone\Mothership;
-use App\Content;
+use App\MetaData;
 
 class FrontPageController extends Controller
 {
@@ -20,11 +22,15 @@ class FrontPageController extends Controller
      */
     public function index()
     {
+        $meta = MetaData::first();
+        SEOMeta::setTitle($meta->title);
+        SEOMeta::setDescription($meta->description);
+
         $realtor = User::where('is_realtor', 1)->exists() ? User::realtor() : null;
+        $content = Content::first();
         if ($realtor) {
             $realtor->listings = Mothership::agentListings($realtor->mls_id);
         }
-        $content = Content::first();
 
         return view('StaticPages.front', compact('realtor', 'content'));
     }
