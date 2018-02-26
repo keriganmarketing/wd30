17e76ce8c85984a7fcc2585fb7e0c2c6c30f81cc
programmer-man <bryan@kerigan.com>
2018-02-26 17:58:10 -0500

    resolves #55

--- resources/views/partials/mini-listing.blade.php
+++ resources/views/partials/mini-listing.blade.php
@@ -12,7 +12,18 @@
                     {{ $listing->status }}
                 </div>
             @endif
-            <img class="w-auto h-auto min-h-full min-w-full" src="{{ $listing->preferred_image }}" >
+            @if(isset($listing->preferred_image))
+                <img class="w-auto h-auto min-h-full min-w-full" src="{{ $listing->preferred_image }}">
+            @else
+                <div class="text-grey-lighter w-full h-full bg-white text-center">
+                    <svg class="h-full " version="1.1" xmlns="http://www.w3.org/2000/svg"
+                         xmlns:xlink="http://www.w3.org/1999/xlink"
+                         x="0px" y="0px" viewBox="0 0 1000 1000"
+                         enable-background="new 0 0 1000 1000" xml:space="preserve">
+                        <path class="fill-current stroke-current opacity-75" d="M200.9,489.4L163.5,444L500,167.2L836.5,444l-37.4,45.4L500,243.4L200.9,489.4z M853.9,645.6h-96.8v93.6H244.2v-93.6h-98.1L10,832.6h54.3c2.5,0,5,0.1,7.5,0.1c120.5,0,240.9,0,361.4,0c6.4,0,12.8,0,19.1,0h111.2c1.2,0,2.2,0,3.4,0c120.5,0,240.9,0.1,361.4,0c2.5,0,5,0,7.5-0.1H990L853.9,645.6z M500,300.2l217.2,179.9v163.7v56.1v6H282.8v-6.7v-55.5V479.4L500,300.2z M496.7,544.3h-80.5v75.2h80.5V544.3z M496.7,459.3h-80.5v75.2h80.5V459.3z M507.2,534.5h80.5v-75.2h-80.5V534.5z M507.2,619.6h80.5v-75.2h-80.5V619.6z" ></path>
+                    </svg>
+                </div>
+            @endif
         </div>
         <div class="property-details py-2 px-2">
             <div class="address h-12 flex flex-col justify-center align-middle">
