ea0abf7fe3fdb8acf0f05129f81ee85c994e3c21
Dylan <johnston94@gmail.com>
2018-03-05 14:30:50 -0600

    Change pagination and filter strucutre

--- resources/assets/js/components/LeadPagination.vue
+++ resources/assets/js/components/LeadPagination.vue
@@ -1,12 +1,11 @@
 <template>
 <div>
-    <ul class="list-reset flex justify-start md:justify-start flex-wrap">
-        <li class="w-full text-sm"
-           v-if="pagination.total != 0">
+    <ul class="list-reset flex justify-start md:justify-start flex-wrap text-md">
+        <li class="w-full text-sm">
            <p class="align-middle font-thin text-smoke-light mb-2 text-center">Showing {{ pagination.from }} - {{ pagination.to }} out of {{ pagination.total }}</p>
         </li>
         <li class="mr-2 w-full flex-1 md:w-auto">
-            <button class="text-grey-darker px-4 py-2 text-center cursor-pointer shadow rounded-sm w-full"
+            <button class="text-grey-darker px-4 py-2 text-center cursor-pointer shadow rounded-sm w-full border border-dbblue-dark"
                :disabled="pagination.prev_page_url == null"
                :class="{
                    'cursor-not-allowed': pagination.prev_page_url == null,
@@ -19,16 +18,16 @@
                    Prev
             </button>
         </li>
-        <li class="mr-2 flex w-1/8 text-center justify-center">
+        <li class="mr-2 flex w-1/8 text-center justify-center border border-dbblue-dark">
             <a class="text-grey-darker bg-brand-lightest px-4 py-2 text-center flex shadow items-center rounded-sm w-full"
             >
-            <span class="text-center">
+            <span class="text-center w-full">
             {{ pagination.current_page }}
             </span>
             </a>
         </li>
         <li class="w-full flex-1 md:w-auto">
-            <button class="text-grey-darker bg-tan-lightest px-4 py-2 text-center cursor-pointer shadow rounded-sm w-full"
+            <button class="text-grey-darker bg-tan-lightest px-4 py-2 text-center cursor-pointer shadow rounded-sm w-full border border-dbblue-dark"
                :disabled="pagination.next_page_url == null"
                :class="{
                    'cursor-not-allowed': pagination.next_page_url == null,
