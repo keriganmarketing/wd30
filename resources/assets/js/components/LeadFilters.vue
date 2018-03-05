d9eed0dd9b71a0f7e16e0f2e2b27d47b24ebbda6
Dylan <johnston94@gmail.com>
2018-03-04 23:18:25 -0600

    Switch filter order.

--- resources/assets/js/components/LeadFilters.vue
+++ resources/assets/js/components/LeadFilters.vue
@@ -1,6 +1,22 @@
 <template>
     <div>
         <div class="switch-container flex-wrap justify-end items-center mx-auto text-center">
+            <div class="w-full md:w-1/2 active-or-inactive-switch flex md:flex-1 ml-0 md:pl-1 mt-1 md:mt-0">
+                <input
+                        type="checkbox"
+                        id="switch_left_2"
+                        @change="toggle('importantFilter')"
+                        :checked="!importantFilter"
+                />
+                <label for="switch_left_2" class="shadow-sm flex-1 mr-1 py-2">All</label>
+                <input
+                        type="checkbox"
+                        id="switch_right_2"
+                        @change="toggle('importantFilter')"
+                        :checked="importantFilter"
+                />  
+                <label for="switch_right_2" class="shadow-sm flex-1 py-2">Important</label>
+            </div>
             <div class="w-full md:w-1/2 active-or-inactive-switch flex md:flex-1 mr-0 md:pr-1 mb-2 md:mb-0">
                 <input
                         type="checkbox"
@@ -18,63 +34,6 @@
                 />
                 <label for="switch_right" class="shadow-sm flex-1 py-2">Inactive</label>
             </div>
-            <div class="w-full md:w-1/2 active-or-inactive-switch flex md:flex-1 ml-0 md:pl-1 mt-1 md:mt-0">
-                <input
-                        type="checkbox"
-                        id="switch_left_2"
-                        @change="toggle('importantFilter')"
-                        :checked="!importantFilter"
-                />
-                <label for="switch_left_2" class="shadow-sm flex-1 mr-1 py-2">All</label>
-                <input
-                        type="checkbox"
-                        id="switch_right_2"
-                        @change="toggle('importantFilter')"
-                        :checked="importantFilter"
-                />  
-                <label for="switch_right_2" class="shadow-sm flex-1 py-2">Important</label>
-            </div>
-
-
-
-            <!--<label class="active-toggle text-white flex-grow w-8 text-right px-2"
-                   :class="{ 'font-bold': activeFilter }" for="activeToggle">
-                Active
-            </label>
-            <div class="switch-box w-8">
-                <label class="sudo-switcher bg-brand-darker" :class="{ 'checked': !activeFilter }" >
-                    <input
-                            type="checkbox"
-                            id="activeToggle"
-                            :checked="!activeFilter"
-                            @change="toggle('activeFilter')"
-                    />
-                </label>
-            </div>
-            <label class="active-toggle text-white flex-grow w-8 px-2" :class="{ 'font-bold': !activeFilter }"
-                   for="activeToggle">
-                Archived
-            </label>
-        </div>
-        <div class="switch-container flex justify-center items-center w-full px-4 py-1">
-            <label class="important-toggle text-white flex-grow w-8 text-right px-2"
-                   :class="{ 'font-bold': !importantFilter }" for="importantToggle">
-                All
-            </label>
-            <div class="switch-box w-8">
-                <label class="sudo-switcher bg-brand-darker" :class="{ 'checked': importantFilter }" >
-                    <input
-                            type="checkbox"
-                            id="importantToggle"
-                            :checked="importantFilter"
-                            @change="toggle('importantFilter')"
-                    />
-                </label>
-            </div>
-            <label class="important-toggle text-white flex-grow w-8 px-2" :class="{ 'font-bold': importantFilter }"
-                   for="importantToggle">
-                Important
-            </label>-->
         </div>
     </div>
 </template>
