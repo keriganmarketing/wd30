4ee4ba4132ebd4aef5d97cd4e9130ab7793c6d72
Bryan Baird <bryan@kerigan.com>
2020-11-13 15:35:15 -0500

    make broker name darker

--- resources/views/partials/logo.blade.php
+++ resources/views/partials/logo.blade.php
@@ -2,6 +2,6 @@
     <span class="realtor-name py-2 mx-1 md:m-0 text-2xl md:text-3xl">
         <span v-if="! boilerplate" class="logo-text-primary font-brand text-2xl md:text-4xl">@{{ user.name }}</span>
         <span v-else class="logo-text-primary font-brand text-2xl md:text-4xl">Your Name</span>
-        <span class="logo-text-secondary block text-xs md:text-sm pb-2">@{{ user.company }}</span>
+        <span class="text-grey-dark block text-xs md:text-sm pb-2">@{{ user.company }}</span>
     </span>
 </a>
