fe444ce2876d8d6a5da790ef21bd621433d6a4ee
Gene Howell <gene@kerigan.com>
2021-01-11 11:50:40 -0600

    added class to photo pages, and added css for photo animation

--- resources/assets/js/components/PropertyPhoto.vue
+++ resources/assets/js/components/PropertyPhoto.vue
@@ -1,29 +1,29 @@
 <template>
-    <div>
-        <div
-            class="w-full is-4by3 cursor-pointer"
-            :style="{ 'background-image': 'url(' + photourl + ')' }"
-            style="background-position:center; background-size:cover;"
-        >
-        <p style="opacity: 0">{{ caption }}</p>
-        </div>
+  <div>
+    <div
+      class="w-full is-4by3 cursor-pointer"
+      :style="{ 'background-image': 'url(' + photourl + ')' }"
+      style="background-position: center; background-size: cover"
+    >
+      <p style="opacity: 0">{{ caption }}</p>
     </div>
+  </div>
 </template>
 <script>
 export default {
-    props: {
-        photoid: {
-            type: Number,
-            default: this.photoid
-        },
-        photourl: {
-            type: String,
-            default: this.photourl
-        },
-        caption: {
-            type: String,
-            default: ''
-        }
-    }
-}
+  props: {
+    photoid: {
+      type: Number,
+      default: this.photoid,
+    },
+    photourl: {
+      type: String,
+      default: this.photourl,
+    },
+    caption: {
+      type: String,
+      default: "",
+    },
+  },
+};
 </script>
--- resources/views/partials/property/photos.blade.php
+++ resources/views/partials/property/photos.blade.php
@@ -1,7 +1,9 @@
 <div class="listing-slider px-1 w-full">
     <img 
         class="w-auto h-auto min-w-full" 
+        lazy="loading"
         src="{{ str_replace('http://','//', $property->preferred_image) }}"  
+        data-src="{{ str_replace('http://','//', $property->preferred_image) }}"
         tabindex="0" 
         alt="Photo of {{ $property->street_number }} {{ $property->street_name }} {{ $property->street_suffix }}"
     >
@@ -9,14 +11,17 @@
 <div class="w-full">
     <property-gallery>
         @foreach($property->photos as $key => $photo)
-            <property-photo
-                tabindex="0"
-                class="w-full sm:w-1/2 md:w-1/6 lg:w-1/4 px-1 py-1 overflow-hidden"
-                :photoid="{{ $key }}"
-                photourl="{{ str_replace('http://','//', $photo->url) }}"
-                caption="Photo {{$key .' of '. count($property->photos) . ' | ' . $property->street_number }} {{ $property->street_name }} {{ $property->street_suffix }}"
-            >
-            </property-photo>
+            <!-- <transition mode="in" enter-active-class="animate__animated animate__fadeIn"> -->
+                <property-photo
+                    tabindex="0"
+                    class="w-full sm:w-1/2 md:w-1/6 lg:w-1/4 px-1 py-1 overflow-hidden lazy-img-fadein animate:transition ease-in duration-700"
+                    lazy="loading"
+                    :photoid="{{ $key }}"
+                    photourl="{{ str_replace('http://','//', $photo->url) }}"
+                    caption="Photo {{$key .' of '. count($property->photos) . ' | ' . $property->street_number }} {{ $property->street_name }} {{ $property->street_suffix }}"
+                >
+                </property-photo>
+            <!-- </transition> -->
         @endforeach
     </property-gallery>
 </div>
